17cb53f747f361ccc07ff3b6449ffaa630301678
jyotisingh301299 <jyotisingh301299@gmail.com>
2021-07-21 16:15:10 +0530

    created directory structure

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+
+const PORT = 8000;
+
+// middlewares
+app.use(bodyParser.json());   //parses the request body
+app.use(cookieParser());
+
+
+app.get("/ram",(req,res)=>{
+    return res.status(200).json({
+        message: "HOME PAGE"
+    });
+});
+
+
+
+
+
+app.listen(PORT, ()=>{
+    console.log("SERVER IS UP AND RUNNING AT PORT ",PORT);
+})
